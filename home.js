0928eb5add84f6edab83cac733c1319d804ee0b3
phamvanthinh9x <phamvanthinh30121996@gmail.com>
2019-10-24 20:04:13 +0700

    cm4

--- home.js
+++ home.js
@@ -2,27 +2,13 @@ $(document).on('ready', function() {
        $('.slider-for').slick({
          autoplay: true,
        });
-
-
-
-       //if (true) {
-      // $(".fomat_fas").click(function(){
-       //$(".bg_top_2").hide();
-       //});
-      // }
 });
-function show_hide()
-{
-
-	var x=$(".bg_top_2").css("display");
-	if (x="block") {
-    $(".bg_top_2").css("display","none");
-    x=$(".bg_top_2").css("display");
-    console.log(x);
-    }
-    else(x="none") {
-    $(".bg_top_2").css("display","block");
-    x=$(".bg_top_2").css("display");
-    }
-
+function menu(){
+  var x=$('#menu').css("display");
+  if (x=="block") {
+      $('#menu').hide();
+  }
+  else {
+    $('#menu').show();
+  }
 }
