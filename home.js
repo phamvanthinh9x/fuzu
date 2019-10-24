709a10d98f915b7691023e4dc2f89aeeff62d127
phamvanthinh9x <phamvanthinh30121996@gmail.com>
2019-10-24 17:57:39 +0700

    cm3

--- home.js
+++ home.js
@@ -2,4 +2,27 @@ $(document).on('ready', function() {
        $('.slider-for').slick({
          autoplay: true,
        });
-    });
+
+
+
+       //if (true) {
+      // $(".fomat_fas").click(function(){
+       //$(".bg_top_2").hide();
+       //});
+      // }
+});
+function show_hide()
+{
+
+	var x=$(".bg_top_2").css("display");
+	if (x="block") {
+    $(".bg_top_2").css("display","none");
+    x=$(".bg_top_2").css("display");
+    console.log(x);
+    }
+    else(x="none") {
+    $(".bg_top_2").css("display","block");
+    x=$(".bg_top_2").css("display");
+    }
+
+}
